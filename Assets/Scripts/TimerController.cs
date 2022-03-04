1b0d5337713c91e6b57973fc91cf612cb68235ad
Chauchauz <chauchauzx@gmail.com>
2022-03-04 12:23:45 +1100

    Coins Implemented

--- Assets/Scripts/TimerController.cs
+++ Assets/Scripts/TimerController.cs
@@ -3,25 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class TimerController : MonoBehaviour
+public class TextController : MonoBehaviour
 {
-    public Text textElement;
+    public Text timerTextElement;
+    public Text coinTextElement;
+
     private float time;
     // Start is called before the first frame update
     void Start()
     {
         time = 999;
+        coinTextElement.text = "x" + 0;
     }
 
     // Update is called once per frame
     void Update()
     {
         time -= Time.deltaTime;
-        displayTime();
+        DisplayTime();
     }
 
-    void displayTime()
+    void DisplayTime()
     {
-        textElement.text = "TIME\n" + (int)time;
+        timerTextElement.text = "TIME\n" + (int)time;
+    }
+
+    public void UpdateCoins(int coins)
+    {
+        coinTextElement.text = "x" + coins;
     }
 }
