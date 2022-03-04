d523ec0a10c081579a6d6e555e07e0713ca8f8fd
nin66 <nina.g162015@gmail.com>
2022-03-04 15:21:46 +1100

    Asset Changes + Camera Script

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private float followSpeed = 2f;
+    [SerializeField] private Transform target;
+
+    private Vector3 basePosition;
+
+    private void Awake()
+    {
+        basePosition = transform.position;
+    }
+
+    private void Update()
+    {
+        LerpToTarget();
+    }
+
+    public void MoveToTarget()
+    {
+        transform.position = TargetPosition();
+    }
+
+    private void LerpToTarget()
+    {
+        transform.position = Vector3.Lerp(transform.position, TargetPosition(), followSpeed * Time.deltaTime);
+    }
+
+    private Vector3 TargetPosition()
+    {
+        return new Vector3(target.position.x, basePosition.y, basePosition.z);
+    }
+}
